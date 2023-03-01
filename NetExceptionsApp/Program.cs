7af4d2b7f953310d13976576bc737867ddeedd60
maxefimov <efimov.academy.top@gmail.com>
2023-03-01 20:21:22 +0300

    Добавьте файлы проекта.

---
+++ NetExceptionsApp/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace NetExceptionsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //if(Int32.TryParse(Console.ReadLine(), out int num))
+            //    Console.WriteLine(num);
+
+            //try
+            //{
+            //    int n = 10;
+            //    int d = Int32.Parse(Console.ReadLine()!);
+            //    int m = n / d;
+
+            //    Console.WriteLine($"Operation!");
+            //}
+            //catch (DivideByZeroException e)
+            //{
+
+            //    Console.WriteLine($"Special Exception! {e.Message}");
+            //}
+            //catch(Exception e) 
+            //{
+            //    Console.WriteLine($"Exception! {e.Message}");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine($"Finally block");
+            //}
+
+            try
+            {
+                //Method();
+                User user = new User();
+                user.Age = -10;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                Console.WriteLine($"{ex.TargetSite}");
+                Console.WriteLine($"{ex.StackTrace}");
+            }
+
+            
+
+            Console.WriteLine($"Program work block");
+        }
+
+        static void Method()
+        {
+            int a = 100;
+            int b = Int32.Parse(Console.ReadLine()!);
+
+            try
+            {
+                Console.WriteLine(a / b);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+                Console.WriteLine($"{ex.TargetSite}");
+                Console.WriteLine($"{ex.StackTrace}");
+                //throw ex;
+            }
+            finally { Console.WriteLine(); }
+        }
+    }
+}
---
+++ NetExceptionsApp/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetExceptionsApp
+{
+
+    public class UserAgeException : ArgumentException
+    {
+        public UserAgeException(string message = "Non correct age value")
+            : base(message)
+        { }
+    }
+    internal class User
+    {
+        int age;
+        public string Name { set; get; }
+        public int Age 
+        { 
+            set
+            {
+                if (value > 0 && value < 100)
+                    age = value;
+                else
+                    throw new UserAgeException();
+            }
+            get => age;
+        }
+
+    }
+}
